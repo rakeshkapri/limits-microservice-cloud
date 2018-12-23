77db9f479f616e9dfd9d77843459fa1712e4f9d8
rakesh kapri <rakeshkapri@gmail.com>
2018-12-23 13:26:41 +0000

    Added Configuration class to read from properties file usin @CongirationProperties

--- src/main/java/com/in28minutes/microservices/limitsservice/controller/LimitsConfigurationController.java
+++ src/main/java/com/in28minutes/microservices/limitsservice/controller/LimitsConfigurationController.java
@@ -1,14 +1,19 @@
 package com.in28minutes.microservices.limitsservice.controller;
 
 import com.in28minutes.microservices.limitsservice.entity.LimitConfigration;
+import com.in28minutes.microservices.limitsservice.propertyconfiguration.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitsConfigurationController {
 
+    @Autowired
+    private Configuration config;
+
     @GetMapping("/limits")
     public LimitConfigration retrieveLimitsFromConfiguration(){
-        return new LimitConfigration(1000, 1);
+        return new LimitConfigration(config.getMaximum(), config.getMinimum());
     }
 }
