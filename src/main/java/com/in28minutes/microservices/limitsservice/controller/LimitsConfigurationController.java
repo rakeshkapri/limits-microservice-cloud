ce964c210dce9aeb7b3772f0a6cd5671964241e2
rakesh kapri <rakeshkapri@gmail.com>
2018-12-23 13:09:23 +0000

    Added Limits controller and Limits POJO

--- src/main/java/com/in28minutes/microservices/limitsservice/controller/LimitsConfigurationController.java
+++ src/main/java/com/in28minutes/microservices/limitsservice/controller/LimitsConfigurationController.java
@@ -1,8 +1,14 @@
 package com.in28minutes.microservices.limitsservice.controller;
 
+import com.in28minutes.microservices.limitsservice.entity.LimitConfigration;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitsConfigurationController {
 
+    @GetMapping("/limits")
+    public LimitConfigration retrieveLimitsFromConfiguration(){
+        return new LimitConfigration(1000, 1);
+    }
 }
