35014361f64b61574d37b30d310be0eb8c5eb204
rakesh kapri <rakeshkapri@gmail.com>
2018-12-23 13:09:06 +0000

    Added Limits controller and Limits POJO

---
+++ src/main/java/com/in28minutes/microservices/limitsservice/entity/LimitConfigration.java
@@ -0,0 +1,15 @@
+package com.in28minutes.microservices.limitsservice.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class LimitConfigration {
+    private int maximum;
+    private int minimum;
+}
