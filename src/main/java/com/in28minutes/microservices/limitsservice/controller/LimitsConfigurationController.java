5dd17ba7078e81e6013b8056ae365a7f58da7d40
rakesh kapri <rakeshkapri@gmail.com>
2018-12-31 13:44:48 +0000

    Added git hystrix dependency for fault tolerance

--- src/main/java/com/in28minutes/microservices/limitsservice/controller/LimitsConfigurationController.java
+++ src/main/java/com/in28minutes/microservices/limitsservice/controller/LimitsConfigurationController.java
@@ -2,6 +2,8 @@ package com.in28minutes.microservices.limitsservice.controller;
 
 import com.in28minutes.microservices.limitsservice.entity.LimitConfigration;
 import com.in28minutes.microservices.limitsservice.propertyconfiguration.Configuration;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.sun.org.apache.xalan.internal.utils.XMLSecurityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,5 +17,15 @@ public class LimitsConfigurationController {
     @GetMapping("/limits")
     public LimitConfigration retrieveLimitsFromConfiguration(){
         return new LimitConfigration(config.getMaximum(), config.getMinimum());
+    }
+
+    @GetMapping("/limits-fault-tolerance")
+    @HystrixCommand(fallbackMethod = "handleLimitsFault")
+    public LimitConfigration retrieveLimitsFaultTolerance(){
+        throw new RuntimeException("run time exception");
+    }
+
+    private LimitConfigration handleLimitsFault(){
+        return new LimitConfigration(5555, 5);
     }
 }
